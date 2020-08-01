5c18ff3aaf3e566a94fb71f85b9b39de3e2cdac4
Antoine Chataignier <antoinechataignier1998@orange.fr>
2020-08-01 13:30:06 +0200

    style/fix: improve style, fix date modification (getDay -> getDate)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var actualDate = new Date();
-var day = actualDate.getDay();
+var day = actualDate.getDate();
 var month = actualDate.getMonth() + 1;
 var year = actualDate.getFullYear();
 var now_date = [Number(day), Number(month), Number(year)];
@@ -22,6 +22,7 @@ if (old_date == null) {
 }
 
 function adapt_date() {
+    localStorage.setItem('old_date', JSON.stringify(now_date));
     let security = 365;
 
     while (old_date != now_date) {
@@ -52,11 +53,13 @@ function Task(title, frequency, begining) {
     this.title = title;
     this.frequency = frequency;
     this.begining = begining;
+    this.isDone = false;
 }
 
 function new_day(show=true) {
     for (let i=0; i<task_list.length; i++) {
         task_list[i].begining--;
+        task_list[i].isDone = false;
         if (task_list[i].begining < 0) {
             task_list[i].begining += task_list[i].frequency;
         }
@@ -77,20 +80,31 @@ function show_task_to_do(task) {
     let task_button = document.createElement("button");
     task_button.innerHTML = "V";
     task_button.onclick = function() {
-        if (this.parentNode.style.backgroundColor == "green") {
-            this.parentNode.style.backgroundColor = "grey";
+        if (this.parentNode.style.backgroundColor == "rgb(0, 186, 0)") {
+            task.isDone = false;
+            this.parentNode.style.backgroundColor = "rgb(161, 0, 161)";
+            this.style.backgroundColor = "rgb(0, 186, 0)";
             this.innerHTML = "V";
         }
         else {
-            this.parentNode.style.backgroundColor = "green";
+            task.isDone = true;
+            this.parentNode.style.backgroundColor = "rgb(0, 186, 0)";
+            this.style.backgroundColor = "rgb(255, 153, 0)";
             this.innerHTML = "annuler";
         }
+        localStorage.setItem('task_list', JSON.stringify(task_list));
+        actualie_label_task_done();
     }
 
     new_task.appendChild(task_title);
     new_task.appendChild(task_button);
 
     document.getElementById("task_list").appendChild(new_task);
+
+    if (task.isDone) {
+        task.isDone = false;
+        task_button.click();
+    }
 }
 
 function show_task_manager(task) {
@@ -119,9 +133,10 @@ function show_task_manager(task) {
 
     let container = document.createElement("div");
     let label = document.createElement("p");
-    label.innerHTML = "Fréquence :";
+    label.innerHTML = "Fréquence";
     let data = document.createElement("p");
     data.innerHTML = "1 jour sur "+String(task.frequency);
+    data.setAttribute("class", "data_task");
 
     let btn_container = document.createElement("div");
     btn_container.setAttribute("class", "value_modifier");
@@ -147,9 +162,10 @@ function show_task_manager(task) {
     
     container = document.createElement("div");
     label = document.createElement("p");
-    label.innerHTML = "Commence dans :";
+    label.innerHTML = "Commence dans";
     data = document.createElement("p");
     data.innerHTML = String(task.begining) + " jour(s)";
+    data.setAttribute("class", "data_task");
 
     btn_container = document.createElement("div");
     btn_container.setAttribute("class", "value_modifier");
@@ -200,17 +216,33 @@ function change_begining_task(elt, qte, child_index, task=null) {
     }
 }
 
+function actualie_label_task_done() {
+    let task_done = 0;
+    let task_total = 0;
+    for (let i=0; i<task_list.length; i++) {
+        if (task_list[i].begining == 0) {
+            task_total++;
+            if (task_list[i].isDone) {
+                task_done++;
+            }
+        }
+    }
+    document.getElementById("nb_task_done").textContent = task_done+"/"+task_total;
+}
+
 function show_to_do_list() {
     document.getElementById("new_task").style.display = "none";
     document.getElementById("task_list").innerHTML = "";
     for (let i=0; i<task_list.length; i++) {
         show_task_to_do(task_list[i]);
     }
-    document.getElementById("btn_task_list").innerHTML = "Gestion des tâches";
+    document.getElementById("nb_task_done").style.display = "block";
+    document.getElementById("btn_task_list").style.backgroundImage = "url(img/parameters.png)";
     document.getElementById("btn_task_list").onclick = function() {
         show_task_list_manager();
     }
     document.getElementById("btn_new_task").style.display = "none";
+    actualie_label_task_done();
 }
 
 function show_task_list_manager() {
@@ -219,13 +251,13 @@ function show_task_list_manager() {
         show_task_manager(task_list[i]);
     }
 
-    document.getElementById("btn_task_list").innerHTML = "Accueil";
+    document.getElementById("nb_task_done").style.display = "none";
+    document.getElementById("btn_task_list").style.backgroundImage = "url(img/home.png)";
     document.getElementById("btn_task_list").onclick = function() {
         localStorage.setItem('task_list', JSON.stringify(task_list));
         show_to_do_list();
     }
     document.getElementById("btn_new_task").style.display = "block";
-    document.getElementById("btn_new_task").innerHTML = "Nouvelle tâche";
     document.getElementById("btn_new_task").onclick = function() {
         document.getElementById("task_list").innerHTML = "";
         document.getElementById("new_task").style.display = "flex";
@@ -251,22 +283,6 @@ function validation_new_task() {
     document.getElementById("new_task").style.display = "none";
     show_task_list_manager();
 }
-
-
-function create_false_day() {
-    now_date[0]++;
-    if (now_date[0] > max_date[now_date[1]-1]) {
-        now_date[0] = 1;
-        now_date[1]++;
-        if (now_date[1] > 12) {
-            now_date[2]++;
-            now_date[1] = 1;
-        }
-    }
-    adapt_date();
-    show_to_do_list();
-}
-
 
 
 adapt_date();
